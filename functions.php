9547b4924f116ad05658017168fea34cb97d7a84
Tom Wahlin <tom.wahlin@gmail.com>
2011-07-29 14:36:04 -0500

    Last FM feed edits
    Feed edits. Make sure to format full-width layout.

--- functions.php
+++ functions.php
@@ -80,5 +80,27 @@ add_filter( 'body_class', 'add_body_class' );
 //end adds body classes to respective pages
 
 
+// Hides posts in admin menu
+add_action('admin_head', 'hide_menus');
+
+function hide_menus() {
+    ?>
+    <style>
+        #menu-posts{ display:none; }
+    </style>
+    <?php
+}
+// End hides posts in admin menu
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
