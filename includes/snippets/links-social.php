f243c7c9c11bd8bb4d478f72182027c1d64f540b
Tom Wahlin <tom.wahlin@gmail.com>
2011-08-21 09:50:30 -0500

    Home page connections

--- includes/snippets/links-social.php
+++ includes/snippets/links-social.php
@@ -5,10 +5,10 @@
 ?>
 
 <ul class="links_social">
-   <li class="dribbble"><a rel="external" title="Visit Tom Wahlin&rsquo;s Dribbble" href="http://dribbble.com/twahlin"><em>Dribbble </em><span>View Shots on Dribbble</span></a></li>
-   <li class="facebook"><a rel="external" title="Visit Tom Wahlin&rsquo;s Facebook" href="http://www.facebook.com/twahlin"><em>Facebook </em><span>Friend on Facebook</span></a></li>
-   <li class="flickr"><a rel="external" title="Visit Tom Wahlin&rsquo;s Flickr" href="http://www.flickr.com/photos/twahlin"><em>Flickr </em><span>View Photos on Flickr</span></a></li>
-   <li class="linkedin"><a rel="external" title="Visit Tom Wahlin&rsquo;s Linkedin" href="http://www.linkedin.com/in/tomwahlin"><em>Linkedin </em><span>Connect <strong class="small_caps">via</strong> Linkedin</span></a></li>
-   <li class="last_fm"><a rel="external" title="Visit Tom Wahlin&rsquo;s Last.fm" href="http://www.last.fm/user/twahlin"><em>Last.fm </em><span>Listen on Last.fm</span></a></li>
-   <li class="twitter"><a rel="external" title="Visit Tom Wahlin&rsquo;s Twitter" href="http://twitter.com/twahlin"><em>Twitter </em><span>Follow on Twitter</span></a></li>
+   <li class="dribbble"><a rel="external" title="Visit Tom Wahlin&rsquo;s Dribbble" href="http://dribbble.com/twahlin"><em class="button">Dribbble </em><span>View Shots on Dribbble</span></a></li>
+   <li class="facebook"><a rel="external" title="Visit Tom Wahlin&rsquo;s Facebook" href="http://www.facebook.com/twahlin"><em class="button">Facebook </em><span>Friend on Facebook</span></a></li>
+   <li class="flickr"><a rel="external" title="Visit Tom Wahlin&rsquo;s Flickr" href="http://www.flickr.com/photos/twahlin"><em class="button">Flickr </em><span>View Photos on Flickr</span></a></li>
+   <li class="linkedin"><a rel="external" title="Visit Tom Wahlin&rsquo;s Linkedin" href="http://www.linkedin.com/in/tomwahlin"><em class="button">Linkedin </em><span>Connect <strong class="small_caps">via</strong> Linkedin</span></a></li>
+   <li class="last_fm"><a rel="external" title="Visit Tom Wahlin&rsquo;s Last.fm" href="http://www.last.fm/user/twahlin"><em class="button">Last.fm </em><span>Listen on Last.fm</span></a></li>
+   <li class="twitter"><a rel="external" title="Visit Tom Wahlin&rsquo;s Twitter" href="http://twitter.com/twahlin"><em class="button">Twitter </em><span>Follow on Twitter</span></a></li>
 </ul>
