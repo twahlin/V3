586bc17413427e82c689716f05aa95cbbdcfe213
Tom Wahlin <tom.wahlin@gmail.com>
2011-08-30 21:20:24 -0500

    Functions Structure

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
-<?php include "includes/admin/custom_meta_boxes.php" ?>        
-<?php include "includes/admin/custom_post_types.php" ?> 
-        
-        
 <?php
+
+/*-----------------------------------------------------------------------------------
+Functions
+-----------------------------------------------------------------------------------*/
+
 // adds body classes to respective pages
 function add_body_class( $classes ) {
     global $post;
@@ -18,11 +19,21 @@ add_filter( 'body_class', 'add_body_class' );
 add_action('admin_head', 'hide_menus');
 
 function hide_menus() {
-    ?>
-    <style>
+    echo
+    '<style>
         #menu-posts{ display:none; }
-    </style>
-    <?php
+    </style>';
 }
 // End hides posts in admin menu
+
+/*-----------------------------------------------------------------------------------
+Includes
+-----------------------------------------------------------------------------------*/
+
+//Add custom meta boxes
+include("includes/admin/custom_meta_boxes.php");
+
+//Add custom post types
+include("includes/admin/custom_post_types.php");
+
 ?>
