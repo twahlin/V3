6542e53d426ba55f17f029c6f70106077e1ef189
Tom Wahlin <tom.wahlin@space150.com>
2011-09-14 09:14:22 -0500

    Continuing JS for meta box toggling

--- scripts/admin/toggle_meta_box_per_post_format.js
+++ scripts/admin/toggle_meta_box_per_post_format.js
@@ -1,46 +1,58 @@
 jQuery( document ).ready( function($) {
-    
+	
+	//do something on page load so radio isn't toggled to trigger event. Hide wyswyg when any format is selected
+	
+	var all_format_divs = "#format_blog_0, #format_blog_gallery, #format_blog_link, #format_blog_image, #format_blog_quote, #format_blog_video"
 
-
-// $( "#post_format_box" ).addClass( "hidden" );
-// 
-// $( "input#post-format-0" ).change( function() {
-// $( "#postdivrich" ).removeClass( "hidden" );
-// $( "#post_format_box" ).addClass( "hidden" );
-// } );
-// 
-// $( "input:not(#post-format-0)" ).change( function() {
-// $( "#postdivrich" ).addClass( "hidden" );
-// $( "#post_format_box" ).removeClass( "hidden" );
-// } );
-// 
-// $( "input[name=\"post_format\"]" ).click( function() {
-// var mydiv = $(this).attr( "id" ).replace( "post-format-", "" );
-// $( "#post_format_box div.inside div" ).addClass("hidden");
-// $( "#post_format_box div.inside div#"+mydiv).removeClass( "hidden" );
-// } );
-
-    // $('#blog_quote, #postdivrich, #postexcerpt').addClass('hidden');
-    
-    // $('#formatdiv input#post-format-0').change( function() {
-    //     $('#blog_quote').hide();
-    //     $('#postdivrich, #postexcerpt').show();
-    // });
-    // 
-    // $('#formatdiv input#post-format-quote').change( function() {
-    //     $('#postdivrich, #postexcerpt').hide();
-    //     $('#blog_quote').show();
-    // });
-    
-    console.log("format_blog_" + $("#formatdiv input:checked").val() + " is checked!");
-    
-    $("#formatdiv input").click(function() {
-      console.log("#format_blog_" + $("#formatdiv input:checked").val());
-      // console.log($("format_blog_").val());
-			// $("#format_blog_").val().hide();
-			// $("#format_blog_" + $("#formatdiv input:checked").val()).hide();
-    });
-
+	$("input#post-format-0").change( function() {
+		$("#postdivrich").removeClass( "hidden" );
+	});
+	
+	$("#formatdiv input:not(#post-format-0)").change( function() {
+		$("#postdivrich").addClass("hidden");
+	});
+	
+	$("#formatdiv input").change(function() {
+		var test= $(this).val();
+		$(all_format_divs).addClass("hidden");
+		$("#format_blog_" + test).removeClass("hidden");
+	});	
+	
+	
+	
+	
+	
+	// $( "#post_format_box" ).addClass( "hidden" );
+	// 
+	// $( "input#post-format-0" ).change( function() {
+	// $( "#postdivrich" ).removeClass( "hidden" );
+	// $( "#post_format_box" ).addClass( "hidden" );
+	// } );
+	// 
+	// $( "input:not(#post-format-0)" ).change( function() {
+	// $( "#postdivrich" ).addClass( "hidden" );
+	// $( "#post_format_box" ).removeClass( "hidden" );
+	// } );
+	// 
+	// $( "input[name=\"post_format\"]" ).click( function() {
+	// var mydiv = $(this).attr( "id" ).replace( "post-format-", "" );
+	// $( "#post_format_box div.inside div" ).addClass("hidden");
+	// $( "#post_format_box div.inside div#"+mydiv).removeClass( "hidden" );
+	// } );
+	// 
+	// $('#blog_quote, #postdivrich, #postexcerpt').addClass('hidden');
+	// 
+	// $('#formatdiv input#post-format-0').change( function() {
+	//     $('#blog_quote').hide();
+	//     $('#postdivrich, #postexcerpt').show();
+	// });
+	// 
+	// $('#formatdiv input#post-format-quote').change( function() {
+	//     $('#postdivrich, #postexcerpt').hide();
+	//     $('#blog_quote').show();
+	// });
+	
+	
 });
 
 
